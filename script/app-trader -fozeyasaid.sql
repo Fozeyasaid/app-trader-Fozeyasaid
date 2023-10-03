ca29917c8d4fbcd3bdbd98bc1bdc0a40649138f0
Fozeyasaid <144164893+Fozeyasaid@users.noreply.github.com>
2023-10-03 11:55:04 -0500

    finial push

--- script/app-trader -fozeyasaid.sql
+++ script/app-trader -fozeyasaid.sql
@@ -1,120 +1,188 @@
-1. a.
- --for app_store_apps
-SELECT name, price,min(price)over()
-FROM app_store_apps
-GROUP BY name,price
-HAVING min(price)>=2.5;
+--1
+SELECT *
+FROM app_store_apps;
 
-
---the minimum price to purchase the right app I got =2.99
---I use from sub_qurries to find the app to purchase by the min price
-
-SELECT name ,price
-FROM 
-	(SELECT name, price,min(price)over()
-	FROM app_store_apps
-	GROUP BY name,price
-	HAVING  min(price) >=2.5) AS lowest_price_appstore
-WHERE price = 2.99;
-
---for play_store_app
-SELECT name, price,min(price)over()
-FROM play_store_apps
-GROUP BY name,price
-HAVING (min(price)::money::numeric)>=2.5;
-
-SELECT name, price
-FROM 
-	(SELECT name, price,min(price)over()
-FROM play_store_apps
-GROUP BY name,price
-HAVING (min(price)::money::numeric)>=2.5)AS lowest_price_playstore
-WHERE price::money::numeric = 10.00;
-
-
---finially combine both app and play store app by union to get all apps to purcahse by min price.
-
-(SELECT name ,price
-FROM 
-	(SELECT name, price,min(price)over()
-	FROM app_store_apps
-	GROUP BY name,price
-	HAVING  min(price) >=2.5) AS lowest_price_appstore
-WHERE price = 2.99)
-
-UNION ALL
-(SELECT name, price::money::numeric
-FROM 
-	(SELECT name, price,min(price)over()
-	FROM play_store_apps
-	GROUP BY name,price
-	HAVING (min(price)::money::numeric)>=2.5)AS lowest_price_playstore
-	WHERE price::money::numeric = 10.00);
-
-
---b. Apps earn $5000 per month on average from in-app advertising and in-app purchases regardless of the price of the app.
- 
- 
--- revenue= $5000per/month 
--- total_profit = revenue _total cost
--- for play store
-SELECT SUM(price::money::numeric) as total_cost
-FROM play_store_apps
-
---using sub_quries		
-	
-SELECT name,
-	(5000*12-(SELECT SUM(price::money::numeric) AS total_cost
-      FROM play_store_apps)) AS profit	
+SELECT *
 FROM play_store_apps;
--- profit from play_stor_apps =48863									
+------------------------
+--filter the app with min price and max rating to get the most profitable app
+SELECT name,price::money::numeric,genres,rating
+FROM play_store_apps
+WHERE ROUND(rating*4)/4 = 5.0 and price::money::numeric < 2.5;
 
---for app_store_apps
+SELECT name,price,primary_genre,rating
+FROM APP_store_apps
+WHERE ROUND(rating*4)/4 = 5.0 and price< 2.5;
 
-SELECT SUM(price) as total_cost
-FROM app_store_apps;
+-- Develop some general recommendations about the price range, genre, content rating, or any other app characteristics that the company should target.
+
+  SELECT name, price::money::numeric,rating, review_count::numeric,
+              -- Calculate the price
+         CASE WHEN price::money::numeric <= 2.5 THEN 25000
+                 WHEN price::money::numeric > 2.5 THEN (price::numeric*10000)
+                 END AS purchase_price,
+            -- Calculate the apps longevity (round rating to the nearest .25 using "ROUND(rating*4)/4)"
+                   ROUND(1 + (ROUND(rating*4)/4*2) * 12, 2) AS longevity
+   FROM play_store_apps
+   WHERE price::money::numeric <= 2.50 
+                AND ROUND(rating*4)/4 = 5.0
+   ORDER BY review_count DESC
+   LIMIT 7;
+																   
+																   
+ SELECT name, price,rating, review_count::numeric,
+              -- Calculate the price
+         CASE WHEN price <= 2.5 THEN 25000
+                 WHEN price > 2.5 THEN (price*10000)
+                 END AS purchase_price,
+            -- Calculate the apps longevity (round rating to the nearest .25 using "ROUND(rating*4)/4)"
+                   ROUND(1 + (ROUND(rating*4)/4*2) * 12, 2) AS longevity
+ FROM app_store_apps
+ WHERE price <= 2.50 
+                AND rating= 5.0
+ ORDER BY review_count DESC
+ LIMIT 7;																   
+	--------------------------															   
+---b. Develop a Top 10 List of the apps that App Trader should buy based on profitability/return on investment as the sole priority.               
+-- use CTE that contains apps with the minimum price and maxium ratings in both tables (the most profitable apps)by combinig union all.
+
+--.25 increase in rating = 6 month increase in longevity
+---.50 increase in rating = 12 month increase in longevity
+---.75 increase in rating = 36 month increase in longevity
+---1 increase in rating = 24 month increase in longevity
+
+---1 increase in rating = 2 years increase in longevity
+(WITH both_tables AS ((SELECT name, 
+                              price::money::decimal,rating,review_count::decimal,
+                              -- Calculate the purchase price
+                              CASE WHEN price::money::decimal <= 2.5 THEN 25000
+                                   WHEN price::money::decimal > 2.5 THEN (price::decimal*10000)
+                                   END AS purchase_price,
+                              -- Calculate the apps longevity (round rating to the nearest .25 using "ROUND(rating*4)/4")
+                              ROUND(1 + (ROUND(rating*4)/4*2) * 12, 2) AS longevity
+                    FROM play_store_apps
+                    -- Filter for apps with the minimum price and maximum rating
+                    WHERE price::money::decimal <= 2.50 
+                          AND ROUND(rating*4)/4 = 5.0)
+     UNION ALL
+                    (SELECT name, price,rating, review_count::numeric,
+                            -- Calculate the purchase price
+                            CASE WHEN price <= 2.5 THEN 25000
+                                 WHEN price > 2.5 THEN (price*10000)
+                                 END AS purchase_price,
+                            -- Calculate the apps longevity (round rating to the nearest .25 using "ROUND(rating*4)/4")
+                            ROUND(1 + (ROUND(rating*4)/4*2) * 12, 2) AS longevity
+                    FROM app_store_apps
+                    WHERE price <= 2.50 
+                          AND ROUND(rating*4)/4 = 5.0))
 
 SELECT name,
-	(5000*12-(SELECT SUM(price) AS total_cost
-      FROM app_store_apps)) AS profit	
-FROM app_store_apps;
-
--- profit from app_stor_apps =47576.41
---I conclude that play store is more profitable than app store.
-
-SELECT rating, 
-	CASE WHEN rating =0 then '1_year_lifespan'
-         WHEN rating =1.0 then '3-years_lifespan'
-	     WHEN rating = 4.0 then '9_years_lifespan' ELSE 'UNKNOWN' END  AS 'longevity'
-FROM play_store_apps
-WHERE round(rating*4)4;
-
-
-(SELECT name,price::money::numeric,genres,content_rating
-FROM play_store_apps
-WHERE rating > 5.0 and price::money::numeric < 2.5)
-INTERSECT
-(SELECT name,price,primary_genre,content_rating
-FROM APP_store_apps
-WHERE rating > 5.0 and price< 2.5);
-
-
+       purchase_price,
+       -- Calculate total_revenue (revenue per month * longevity)
+       2500 * longevity AS total_revenue,
+       -- Calculate total_profit (total revenue - purchase price)
+       (2500 * longevity) - purchase_price AS total_profit
+FROM both_tables
+ORDER BY review_count DESC
+LIMIT 10);																   
 																   
-WITH apps_stores AS (SELECT name,price,rating
-			         from app_store_apps
-                     where price<=2.5 AND ROUND(RATING*4)/4=5.0)
-UNION ALL
-				(SELECT NAME,PRICE::MONEY::numeric,RATING
-					from play_store_apps
-					where price::money::numeric<=2.5 and round(rating*4)/4=5.0))
-														
-														
-select *
-from app_store
-where name (select name from play_store_apps)
-intersect 
-select name 
-from app_store_apps);
+--Each of these apps will cost 2,500 and have a lifespan of 121 months the smallest possible cost, and the highest possible lifespan															   
+----------
+ -- c. Develop a Top 4 list of the apps that App Trader should buy that are profitable but that also are thematically appropriate for the upcoming Halloween themed campaign.
+--by using CTE and wild cards.
+																   
+WITH both_tables AS ((SELECT name,price,rating,review_count::numeric
+			        FROM app_store_apps
+                    WHERE price<=2.5 
+					   AND ROUND(rating*4)/4=5.0)
+                    UNION ALL
+				   (SELECT name,PRICE::MONEY::numeric,rating,review_count::numeric
+					FROM play_store_apps
+					WHERE price::money::numeric<=2.5 
+					     AND round(rating*4)/4=5.0))
+SELECT *
+FROM both_tables
+WHERE name ILIKE '% Halloween%' OR
+      name ILIKE '% zombie%' OR
+	  name ILIKE '% spookey%' OR
+	  name ILIKE '% ghost%' OR
+	  name ILIKE '% scary%' OR
+	  name ILIKE '%creepy%'	  
+ORDER BY review_count DESC
+LIMIT 4;															   
+																   
+																   
+																   
+																   
+																   
+																   
+												   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
+																   
 																   
 																   
 																   
